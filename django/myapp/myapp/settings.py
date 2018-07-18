f7982cb1adb7168d409b71bde0409ffc06e7ca7f
abhishekpatra002 <abhishekpatra002@gmail.com>
2018-07-18 15:27:50 +0530

    update django

--- django/myapp/myapp/settings.py
+++ django/myapp/myapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'myapp/template')],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
